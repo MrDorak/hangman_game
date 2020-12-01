e9ba5ba59c602ea3b6ade415adc6572ec398fdfc
MrDorak <dorianturle@hotmail.fr>
2020-12-01 18:44:29 +0100

    component rename && cleanup

---
+++ src/views/Game.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <canvas ref="canva" style="border: 1px solid black;">
+      Désolé, votre navigateur ne prend pas en charge &lt;canvas&gt;.
+    </canvas>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Game',
+  data: () => ({
+    canva: null,
+  }),
+  mounted () {
+    this.initCanva()
+  },
+  methods: {
+    initCanva () {
+    const canva = this.$refs.canva
+    canva.getContext('2d');
+    }
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <GameSettings />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import GameSettings from '@/components/GameSettings.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    GameSettings
   }
 }
 </script>
